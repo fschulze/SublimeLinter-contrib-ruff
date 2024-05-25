e5a1a18f886583dfeffdea931c6fc8fd8939c3ab
Florian Schulze <mail@florian-schulze.net>
2024-05-25 08:32:24 +0200

    Add current file name to enable per-file-ignores.
    Solution from https://github.com/SublimeLinter/SublimeLinter-flake8/issues/113

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter
 
 
 class Ruff(Linter):
-    cmd = ('ruff', '-')
+    cmd = ('ruff', 'check', '--stdin-filename', '${file:stdin}', '-')
     regex = r'^(?P<file>.+):(?P<line>\d+):(?P<col>\d+): (?P<message>.+)$'
     multiline = False
     defaults = {
